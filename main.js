e67f9b3179987dc41ab6541b662f7f7534a1ef91
Your Name <you@example.com>
2022-08-09 20:48:55 +0100

    npm packages added for prompt.

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 
+// It's all about variable scoping. Variables declared in the self executing function are, by default, only available to code within the self executing function. This allows code to be written without concern of how variables are named in other blocks of JavaScript code.
+
+// self executing function here
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+    
+    //npm install prompt-sync
+const prompt = require('prompt-sync')();
+    const quote = prompt("");
+
+const author = prompt("");
+
+})();
 
